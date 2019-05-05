f281449f054317c4c5482ad20f3b611ad7bd0553
Charles Brain <chbrain@btinternet.com>
2019-05-05 16:21:43 +0100

    fread changes to read

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ void my_handler(int s){
 	m_running = false;
 	express_receive();
 	express_deinit();
-//    printf("Caught signal %d\n",s);
+    printf("Caught signal %d\n",s);
     exit(0); 
 }
 
@@ -36,6 +36,15 @@ void printcon( const char *fmt, ... )
     va_start(ap,fmt);
     vprintf(fmt,ap);
     va_end(ap);
+}
+
+void read_in_bytes( uint8_t *b, int len ){
+    int to_read = len;
+    int been_read = 0;
+    while(to_read > 0 ){
+        been_read += read(STDIN_FILENO,&b[been_read],to_read);
+        to_read = len - been_read;
+    }
 }
 
 int main( int c, char *argv[]){
@@ -57,13 +66,12 @@ int main( int c, char *argv[]){
             express_transmit();
             m_running = true;
 	    	while(m_running == true){
-		    	if(fread(buffer,TSPLEN,1,stdin) == TSPLEN){
-            	    if(buffer[0] == 0x47){
+                read_in_bytes( buffer, TSPLEN );
+           	    if(buffer[0] == 0x47){
 						m_dvbs2.s2_add_ts_frame( buffer );
-            	    }else{
-            		    // Not sync so move on 1 byte
-        			    fread(buffer,1,1,stdin);
-            	    }
+            	}else{
+            	    // Not in sync so move on 1 byte
+                    read_in_bytes( buffer, 1 );
 		    	}
 	    	}
 		}else{
@@ -74,3 +82,52 @@ int main( int c, char *argv[]){
 	}
 	return 0;
 }
+/*
+static unsigned char m_null[TP_SIZE];
+
+//
+// Format a transport packet
+//
+void null_fmt( void )
+{
+    m_null[0] = 0x47;
+    m_null[1] = 0;
+    // Add the 13 bit pid
+	m_null[1] = 0x1F;
+	m_null[2] = 0xFF;
+	m_null[3] = 0x10;
+    for( int i = 4; i < TP_SIZE; i++ ) m_null[i] = 0xFF;
+}
+
+int main( int c, char *argv[]){
+	uint8_t buffer[TSPLEN];
+    buffer[0] = 0;
+	struct sigaction sigIntHandler;
+
+	sigIntHandler.sa_handler = my_handler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+
+	null_fmt();
+
+    if( express_init( "datvexpress16.ihx", "datvexpressraw16.rbf" ) == EXP_OK){
+ 		
+		DVB2FrameFormat fmt;
+        process_command_line( c, argv, &fmt);
+   		if(m_dvbs2.s2_set_configure( &fmt ) == 0){
+    		m_dvbs2.s2_register_tx(express_write_16_bit_samples);
+            express_transmit();
+            m_running = true;
+	    	while(m_running == true){
+			    m_dvbs2.s2_add_ts_frame( m_null );
+	    	}
+		}else{
+			printcon( "Error: unable to configure S2 invalid parameter" );
+		}
+	}else{
+		printcon( "Error: unable to init Express" );
+	}
+	return 0;
+}
+*/
